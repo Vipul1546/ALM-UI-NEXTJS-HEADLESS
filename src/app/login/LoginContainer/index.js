7e52b098902ac4e6908f1f94b96923234e845b6e
vipult2022 <116866755+vipult2022@users.noreply.github.com>
2023-10-10 20:40:08 +0530

    env update

--- src/app/login/LoginContainer/index.js
+++ src/app/login/LoginContainer/index.js
@@ -9,13 +9,15 @@ import styles from './loginContainer.module.scss';
 const LoginContainer = () => {
     const router = useRouter()
 
+    const { NEXT_PUBLIC_REDIRECT_DOMAIN, } = process.env
+
     const getAuthCode = async () => {
-        const { NEXT_PUBLIC_ALM_CLIENT_ID } = process.env
-        const url = `https://learningmanager.adobe.com/oauth/o/authorize?${new URLSearchParams({
+        const { NEXT_PUBLIC_ALM_CLIENT_ID, SSO_ADOBE_AUTH_URL, ACCOUNT_ID, lEARNER_SCOPE } = process.env
+        const url = `${SSO_ADOBE_AUTH_URL}?${new URLSearchParams({
             client_id: NEXT_PUBLIC_ALM_CLIENT_ID,
-            redirect_uri: 'https://local-alm-ui-nextjs-headless.vercel.adobe.com/auth',
-            account: 116411,
-            scope: 'learner:read,learner:write',
+            redirect_uri: NEXT_PUBLIC_REDIRECT_DOMAIN,
+            account: ACCOUNT_ID,
+            scope: lEARNER_SCOPE,
             response_type: 'CODE'
         })}`
         window.location.replace(url);
